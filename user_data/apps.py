c2856c228dfe80df8bf27a076fd98dd05fe10cb3
arthur.barbosa <arthur.barbosa@mds.gov.br>
2024-09-05 12:58:31 -0300

    delete all

--- user_data/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class UserDataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_data'
-
-    def ready(self):
-        import user_data.signals  # Importa sinais para garantir que sejam registrados
