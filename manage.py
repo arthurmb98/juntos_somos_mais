69712407686f786e3c5dc24067960183b7a4c706
arthur.barbosa <arthur.barbosa@mds.gov.br>
2024-09-05 12:45:38 -0300

    Populando base ao iniciar aplicação

--- manage.py
+++ manage.py
@@ -2,15 +2,27 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.core.management import execute_from_command_line
-from database import engine
+import django
 
 
-if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'juntos_somos_mais.settings')
-    try:
-        execute_from_command_line(sys.argv)
-    except Exception as e:
-        print(f"Error: {e}")
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'juntos_somos_mais.settings')  # Substitua 'your_project' pelo nome do seu projeto
+    django.setup()
+
+    from user_data.models import User
+    from user_data.load_data import populate_json, populate_csv
+    # Limpar o banco de dados
+    User.objects.all().delete()
+    # Popula o banco de dados
+    populate_json()
+    populate_csv()
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
 
 
